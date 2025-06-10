64a136cc5f7b73346ec0511dd8fdf35a1726e6ba
m-zikry-fadillah <adrylmzf@gmail.com>
2025-06-10 13:43:17 +0800

    initial commit

---
+++ src/latihan2/DataDiri.java
@@ -0,0 +1,22 @@
+package latihan2;
+
+public class DataDiri {
+    // Atribut
+    String nama;
+    String npm;
+    String kelas;
+
+    // Konstruktor
+    public DataDiri(String nama, String npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+
+    // (Opsional) Tambahkan method untuk menampilkan data
+    public void tampilkanData() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NPM: " + npm);
+        System.out.println("Kelas: " + kelas);
+    }
+}
