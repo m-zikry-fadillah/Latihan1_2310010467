0e60ebbeee610948ef05d033d28ec71ee2c1370e
m-zikry-fadillah <adrylmzf@gmail.com>
2025-06-24 03:44:36 +0800

    membuat objek

--- src/latihan2/DataDiri.java
+++ src/latihan2/DataDiri.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan2;
 
-/**
- *
- * @author User
- */
 public class DataDiri {
-    
+    // Atribut
+    String nama;
+    String npm;
+    String kelas;
+
+    // Konstruktor
+    public DataDiri(String nama, String npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
 }
