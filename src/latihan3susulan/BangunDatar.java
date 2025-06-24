e046ec7830ae806bd702eeae0a7c9d118362a253
m-zikry-fadillah <adrylmzf@gmail.com>
2025-06-24 16:41:38 +0800

    initial commit

---
+++ src/latihan3susulan/BangunDatar.java
@@ -0,0 +1,11 @@
+package latihan3susulan;
+
+/**
+ * Nama  : Muhammad Zikry Fadillah
+ * NPM   : 2310010467
+ * Kelas : 4A Reguler Banjarmasin
+ */
+
+public class BangunDatar {
+    // Ini adalah class kosong sebagai awal (initial commit)
+}
