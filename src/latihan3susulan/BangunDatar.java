bba597cf70880db769ba1a09fa4935d6c5338acf
m-zikry-fadillah <adrylmzf@gmail.com>
2025-06-24 16:55:48 +0800

    membuat accessor dan constructor

--- src/latihan3susulan/BangunDatar.java
+++ src/latihan3susulan/BangunDatar.java
@@ -1,9 +1,9 @@
 package latihan3susulan;
 
 /**
- * Nama  : Ziyy Fall
- * NPM   : 202143501234
- * Kelas : TI-2B
+ * Nama  : Muhammad Zikry Fadillah
+ * NPM   : 2310010467
+ * Kelas : 4A Reguler Banjarmasin
  */
 
 public class BangunDatar {
@@ -13,28 +13,48 @@ public class BangunDatar {
     double jariJari;
     double hasil;
 
-    // Method untuk menghitung luas segitiga
+    // Constructor untuk inisialisasi semua atribut ke nol
+    public BangunDatar() {
+        alas = 0;
+        tinggi = 0;
+        jariJari = 0;
+        hasil = 0;
+    }
+
+    // Method mutator untuk menghitung luas segitiga
     public void hitungLuasSegitiga() {
         hasil = 0.5 * alas * tinggi;
         System.out.println("Luas Segitiga = " + hasil);
     }
 
-    // Method untuk menghitung luas lingkaran
+    // Method mutator untuk menghitung luas lingkaran
     public void hitungLuasLingkaran() {
         hasil = 3.14 * jariJari * jariJari;
         System.out.println("Luas Lingkaran = " + hasil);
     }
 
-    // Main method untuk menjalankan program
+    // Method accessor untuk mengembalikan nilai hasil
+    public double getHasil() {
+        return hasil;
+    }
+
+    // Main method untuk pengujian
     public static void main(String[] args) {
         BangunDatar bd = new BangunDatar();
 
-        // Mengisi nilai atribut
-        bd.alas = 8;
+        // Uji nilai awal (semua harus 0)
+        System.out.println("Nilai awal atribut:");
+        System.out.println("Alas = " + bd.alas);
+        System.out.println("Tinggi = " + bd.tinggi);
+        System.out.println("Jari-jari = " + bd.jariJari);
+        System.out.println("Hasil = " + bd.getHasil());
+
+        // Set nilai untuk perhitungan
+        bd.alas = 10;
         bd.tinggi = 5;
         bd.jariJari = 7;
 
-        // Memanggil method untuk menghitung dan menampilkan hasil
+        // Hitung dan cetak hasil
         bd.hitungLuasSegitiga();
         bd.hitungLuasLingkaran();
     }
