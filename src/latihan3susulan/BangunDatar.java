927cf11a8869a638c7a27433f5bbe6acf882d8fd
m-zikry-fadillah <adrylmzf@gmail.com>
2025-06-24 16:50:28 +0800

    membuat mutator

--- src/latihan3susulan/BangunDatar.java
+++ src/latihan3susulan/BangunDatar.java
@@ -12,4 +12,30 @@ public class BangunDatar {
     double tinggi;
     double jariJari;
     double hasil;
+
+    // Method untuk menghitung luas segitiga
+    public void hitungLuasSegitiga() {
+        hasil = 0.5 * alas * tinggi;
+        System.out.println("Luas Segitiga = " + hasil);
+    }
+
+    // Method untuk menghitung luas lingkaran
+    public void hitungLuasLingkaran() {
+        hasil = 3.14 * jariJari * jariJari;
+        System.out.println("Luas Lingkaran = " + hasil);
+    }
+
+    // Main method untuk menjalankan program
+    public static void main(String[] args) {
+        BangunDatar bd = new BangunDatar();
+
+        // Mengisi nilai atribut
+        bd.alas = 8;
+        bd.tinggi = 5;
+        bd.jariJari = 7;
+
+        // Memanggil method untuk menghitung dan menampilkan hasil
+        bd.hitungLuasSegitiga();
+        bd.hitungLuasLingkaran();
+    }
 }
